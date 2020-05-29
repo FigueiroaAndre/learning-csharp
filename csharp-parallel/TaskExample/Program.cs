0903ed6236eb2c1a54c4eb007995785d1a067c29
Andre Figueiroa <alfigueiroab@gmail.com>
2020-05-29 16:49:07 -0300

    Add example to practice Task Programming

---
+++ csharp-parallel/TaskExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    class Program
+    {
+        public static int Counter(int maxValue,int sleepDuration, CancellationToken token)
+        {
+            int counter;
+            System.Console.WriteLine($"{Task.CurrentId} Initialized.");
+            for(counter = 0; counter <= maxValue; counter++)
+            {
+                token.ThrowIfCancellationRequested();
+                System.Console.WriteLine($"{Task.CurrentId}: Counter value = {counter}");
+                Thread.Sleep(sleepDuration);
+            }
+            System.Console.WriteLine($"{Task.CurrentId}: Task Completed.");
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            var source = new CancellationTokenSource();
+            var source2 = new CancellationTokenSource();
+            var counterTask = new Task<int>(() => Counter(10,3000,source.Token));
+            var counter2Task = new Task<int>(() => Counter(15,1000,source2.Token));
+            counterTask.Start();
+            counter2Task.Start();
+            try
+            {
+                Thread.Sleep(10000);
+                source2.Cancel();
+                Task.WaitAll(counterTask,counter2Task);
+            }
+            catch (AggregateException aggregateException)
+            {
+                foreach(var exception in aggregateException.InnerExceptions)
+                {
+                    System.Console.WriteLine($"Main: An {exception.GetType()} have been caught.");
+                }
+            }
+            source.Dispose();
+            source2.Dispose();
+        }
+    }
+}

979ec21d733b21b04beaa0b45ac21a3a15f4ae20
Andre Figueiroa <alfigueiroab@gmail.com>
2020-06-01 14:06:46 -0300

    Add example of regular queue with multithread
    This  example  is expected to rise an error. Regular collection have not
    been design to work in a multithreaded context.

---
+++ csharp-parallel/ConcurrentStructuresExample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentStructuresExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("This program is expected to raise an error!");
+            System.Console.WriteLine("Ordinary collections were not meant to work with multithreading");
+            System.Console.WriteLine("At some point, the dequeue task will try to dequeue an value that haven't been enqueued yet! Looking for the last dequeue print in this execution.");
+            System.Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            var cts = new CancellationTokenSource();
+            var consolePadlock = new object();
+            var queue = new Queue<int>();
+            var token = cts.Token;
+            var task1 = new Task(() => {
+                int i = 0;
+                while(true){
+                    token.ThrowIfCancellationRequested();
+                    queue.Enqueue(i);
+                    System.Console.WriteLine($"{i} enqueued!");
+                    i++;
+                }
+            });
+
+            var task2 = new Task(() => {
+                while(true)
+                {
+                    token.ThrowIfCancellationRequested();
+                    int dequeued = queue.Dequeue();
+                    System.Console.WriteLine($"{dequeued} dequeued!");
+                }
+            });
+
+            task1.Start();
+            Thread.Sleep(1);
+            task2.Start();
+            try
+            {
+                task2.Wait();
+            }
+            catch (Exception e)
+            {
+                cts.Cancel();
+                System.Console.WriteLine("An error has ocurred, the cancelation token cancelled all the Tasks!");
+                System.Console.WriteLine($"ERROR: {e.GetType()} - {e.Message}");
+
+            }
+            finally
+            {
+                task1.Dispose();
+                task2.Dispose();
+            }
+
+            System.Console.WriteLine("Main Done!");
+            Console.ReadKey();
+        }
+    }
+}
